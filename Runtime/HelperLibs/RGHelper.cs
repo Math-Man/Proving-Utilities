0c8e796b67c6d436c457d3cde4f13bc665a2df49
Göktuğ Kayacan <kayacan.goktug@gmail.com>
2021-10-14 05:38:16 +0300

    added params to UniformPointsOnSphere

--- Runtime/HelperLibs/RGHelper.cs
+++ Runtime/HelperLibs/RGHelper.cs
@@ -53,29 +53,32 @@ public static class RGHelper
         return (rp - Center).normalized * radius;
     }
 
-    public static Vector3[] UniformPointsOnSphere(int numberOfPoints)
+    public static Vector3[] UniformPointsOnSphere(int n, float radius, Vector3 offset)
     {
         List<Vector3> upts = new List<Vector3>();
         float inc = Mathf.PI * (3 - Mathf.Sqrt(5));
-        float off = 2.0f / numberOfPoints;
+        float off = 2.0f / n;
         float x = 0;
         float y = 0;
         float z = 0;
         float r = 0;
         float phi = 0;
-       
-        for (var k = 0; k < numberOfPoints; k++){
+
+        for (var k = 0; k < n; k++){
             y = k * off - 1 + (off /2);
             r = Mathf.Sqrt(1 - y * y);
             phi = k * inc;
             x = Mathf.Cos(phi) * r;
             z = Mathf.Sin(phi) * r;
-           
-            upts.Add(new Vector3(x, y, z));
+            Debug.DrawLine(transform.position, offset + new Vector3(x, y, z)*radius);
+            upts.Add(offset + new Vector3(x, y, z)*radius);
         }
         Vector3[] pts = upts.ToArray();
         return pts;
     }
+    
+
+    
     
     public static List<RaycastHit> CircularRaycastOnXY(Vector3 origin, int rayCount, float maxDistance, int layerMask)
     {
