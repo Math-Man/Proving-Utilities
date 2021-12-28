0901ad7ee65bbb430e580aa5769c9eaaf3cd86bc
Göktuğ Kayacan <kayacan.goktug@gmail.com>
2021-12-28 17:42:36 +0300

    IEnumerableExtension for weighted random selection
    IEnumerableExtension for weighted random selection

---
+++ Runtime/IEnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace World_Scripts
+{
+    public static class IEnumerableExtensions
+    {
+        public static T RandomElementByWeight<T>(this IEnumerable<T> sequence, Func<T, float> weightSelector)
+        {
+            float totalWeight = sequence.Sum(weightSelector);
+            float itemWeightIndex =  (float)new Random().NextDouble() * totalWeight;
+            float currentWeightIndex = 0;
+            foreach(var item in from weightedItem in sequence select new { Value = weightedItem, Weight = weightSelector(weightedItem) }) 
+            {
+                currentWeightIndex += item.Weight;
+                if(currentWeightIndex >= itemWeightIndex)
+                    return item.Value;
+            }
+            return default(T);
+        }
+    }
+}
